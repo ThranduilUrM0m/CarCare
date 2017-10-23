0fd4332fdaf299dcab75c0d855b3821c0ae2f130
Boutaleb Zakariae <zakariaeboutaleb@gmail.com>
2017-10-23 22:44:03 +0100

    dunno

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-/**
- * A counter button: tap the button to increase the count.
- */
 class Homepage extends React.Component {
   constructor() {
     super();
   }
-
   render() {
     return (
       <h1>OK</h1>
